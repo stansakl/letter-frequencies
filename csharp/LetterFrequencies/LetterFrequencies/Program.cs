ba01aa3c7a93ec37f6a0ac40a26016ae7716572a
Stan Sakl <stansakl@gmail.com>
2018-01-12 21:18:42 -0600

    Add Usage()

--- csharp/LetterFrequencies/LetterFrequencies/Program.cs
+++ csharp/LetterFrequencies/LetterFrequencies/Program.cs
@@ -7,6 +7,21 @@ namespace LetterFrequencies
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            if(args.Length == 1)
+            {
+                Usage();
+            }
+        }
+
+        static void CountLetters(string filename)
+        {
+
+        }
+
+        static void Usage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("\tLetterFrequencies <file name>");
         }
     }
 }
