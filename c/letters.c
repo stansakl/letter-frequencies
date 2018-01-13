b2558b96d1c86fa89b18eb509bc5a8cc0585fdc2
Stan Sakl <stansakl@gmail.com>
2018-01-12 19:48:36 -0600

    Implement usage(). Look at arguments.

--- c/letters.c
+++ c/letters.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include "letters.h"
 
-void main (int argc, char** argv) {
+void main (int argc, char** argv) 
+{
+    if (argc <=1) {
+        usage();
+        exit();
+    }
 
+    for(int i = 0; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
+}
+
+void usage() {
+    printf("Usage:\n");
+    printf("\tletters <file name>\n");
 }
